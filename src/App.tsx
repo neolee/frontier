464beb45578bf16ae0bef49cd4aca09691d3b042
Neo Lee <neo.lee@gmail.com>
2022-10-07 14:01:11 +0800

    tweak: play with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,19 @@ import styles from './App.module.css';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
+    <div class="text-center">
+      <header class="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white">
         <img src={logo} class={styles.logo} alt="logo" />
-        <p class="text-3xl text-red-400 tracking-widest">
+        <p class="text-5xl font-thin py-10">
+          Welcome to the REAL world!
+        </p>
+        <p class="text-3xl text-red-400 tracking-widest pb-10">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p><a class="text-2xl text-purple-600" href="https://github.com/solidjs/solid"
+              target="_blank" rel="noopener noreferrer">
           Learn Solid
-        </a>
+        </a></p>
       </header>
     </div>
   );
