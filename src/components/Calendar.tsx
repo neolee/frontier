46791359167d1b21b292565c40b5984fc951add3
Neo Lee <neo.lee@gmail.com>
2022-10-08 16:36:10 +0800

    feat: copy calendar text to clipboard

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -37,9 +37,14 @@ const Calendar: Component = () => {
     });
   });
 
-  const onClick: JSX.EventHandler<HTMLButtonElement, MouseEvent> = () => {
+  const onGo: JSX.EventHandler<HTMLButtonElement, MouseEvent> = () => {
     const query = moment(date(), 'YYYY-MM-DD').format('YYYYMMDD');
     setQuery(query);
+  };
+
+  const onCopy: JSX.EventHandler<HTMLButtonElement, MouseEvent> = () => {
+    const display = document.getElementById('display') as HTMLElement;
+    copy(display.innerText);
   };
 
   return (
@@ -57,10 +62,11 @@ const Calendar: Component = () => {
           <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
         </div>
         <input id="datepicker" type="text" value={date()} class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" />
-        <button onMouseDown={onClick} type="button" class="text-white absolute right-5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-6 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Go!</button>
+        <button onMouseDown={onGo} type="button" class="text-white absolute right-5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-6 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Go!</button>
       </div>
       <div class="relative px-2 py-4">
-        <pre class="bg-gray-600 text-red-100 space-y-5">{renderCalendar(calendar())}</pre>
+        <pre id="display" class="bg-gray-600 text-red-100 space-y-5">{renderCalendar(calendar())}</pre>
+        <button onMouseDown={onCopy} type="button" hidden={(!calendar())} class="text-white absolute right-3 top-7 bg-gray-800 border border-gray-300 focus:outline-none hover:text-gray-800 hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-6 py-2 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Copy</button>
       </div>
     </div>
   );
